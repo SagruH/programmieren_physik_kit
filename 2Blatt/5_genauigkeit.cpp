3fd8934a23ff1d4027f6d737974692ddc66c8323
SagruH <sagru2899@googlemail.com>
2020-04-30 23:13:09 +0200

    Blatt2: Aufgabe5

--- 2Blatt/5_genauigkeit.cpp
+++ 2Blatt/5_genauigkeit.cpp
@@ -1,6 +1,7 @@
 
 // github: https://github.com/SagruH/programmieren_physik_kit
 
+#include <iostream>
 #include <cmath>
 #include <chrono>
 
@@ -11,10 +12,21 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
+  double x = 1, e = 2, xe;
+  int i = -1;
 
+  do {
 
-  //code
+    e = e/2;
+    xe = x+e;
+    i++;
 
+  } while(x != xe);
+
+  cout << "1 + \u03B5 \u2260 1 bis \u03B5 = "
+  << 2*e << " Dies ist nach " << i << " halbierungen erreicht.\n";
+
+  // epsilon ist \u03B5 ; ungleich zeichen inst \u2260
 
 
   auto t_end = chrono::high_resolution_clock::now();
