9e2fd78a241eedb5280f5529c6ceea9ec12b62a1
SagruH <sagru2899@googlemail.com>
2020-05-01 00:47:38 +0200

    Blatt2: Aufgabe6 -broken -debug version

--- 2Blatt/6_base_n_count.cpp
+++ 2Blatt/6_base_n_count.cpp
@@ -4,15 +4,34 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
-
+#include <string>
 
 using namespace std;
 
 
+string base_converter(int div, int base) {
+  string r_str, baseXnum = "";
+  int r;
+  cout << "div  " << div << endl;
+  do {
+    div = div / base;
+    r = div % base;
+
+    if (r==0) { break; }
+
+
+    r_str = to_string(r);
+    cout << "r_str  " <<  r_str << endl;
+    baseXnum = r_str + baseXnum;
+    cout << "baseXnum  " << baseXnum << endl;
+  } while(r != 0);
+
+  return baseXnum;
+}
+
 int main(int argc, char const *argv[]) {
-
-
   int base, n;
+  string baseXnum;
 
   cout << "Count in Base: ";
   cin >> base;
@@ -21,7 +40,12 @@ int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now(); //runtime after user input
 
   for (int i = 0; i <= n; i++) {
-    /* code */
+    if (i==0) { baseXnum = "0"; }
+    else {
+    baseXnum = base_converter(i,base);
+    }
+
+    cout << baseXnum << "[" << base << "]  =  " << i << "[10]" << endl;
   }
 
   auto t_end = chrono::high_resolution_clock::now();
