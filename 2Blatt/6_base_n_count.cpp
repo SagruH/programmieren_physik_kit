ef5f5b6fb63613e4d9e995334ebc022476a2a7da
SagruH <sagru2899@googlemail.com>
2020-04-30 23:45:12 +0200

    Blatt2: Aufgabe6 Anfang

--- 2Blatt/6_base_n_count.cpp
+++ 2Blatt/6_base_n_count.cpp
@@ -10,17 +10,23 @@ using namespace std;
 
 
 int main(int argc, char const *argv[]) {
-  auto t_start = chrono::high_resolution_clock::now();
 
 
+  int base, n;
 
-  
+  cout << "Count in Base: ";
+  cin >> base;
+  cout << "Count till (input in Base 10): ";
+  cin >> n;
+  auto t_start = chrono::high_resolution_clock::now(); //runtime after user input
 
-
+  for (int i = 0; i <= n; i++) {
+    /* code */
+  }
 
   auto t_end = chrono::high_resolution_clock::now();
   chrono::duration<double> runtime = t_end - t_start; // runtime calc
-  cout << "Runtime: " << runtime.count() << "s\n";
+  cout << endl << "Runtime: " << runtime.count() << "s\n";
 
   return 0;
 }
