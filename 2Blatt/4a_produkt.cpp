263a7c9cdee5f08b94671123413b94702ba1bd71
SagruH <sagru2899@googlemail.com>
2020-04-30 14:15:01 +0200

    Blatt 2: Aufgabe4a

---
+++ 2Blatt/4a_produkt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  int produkt, n;
+
+  cout << "Enter N: ";
+  cin >> n;
+
+  for(int i = 1 ; i<=N ; i++) {
+
+    cout << "For i = " << i-1 << " : Produkt = " << produkt << endl;
+    produkt = produkt + (3*i - 1)
+
+  }
+
+  cout << "Final result = " << produkt << endl;
+
+  return 0;
+}
