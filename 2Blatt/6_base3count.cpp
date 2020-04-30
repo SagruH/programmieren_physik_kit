77204e5a51b86c3a1c6e08eb5c991c14fcbbf411
SagruH <sagru2899@googlemail.com>
2020-04-30 22:22:17 +0200

    Template update

--- 2Blatt/6_base3count.cpp
+++ 2Blatt/6_base3count.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
+
 
 using namespace std;
 
+
 int main(int argc, char const *argv[]) {
+  auto t_start = chrono::high_resolution_clock::now();
+
+
+
+  //code
+
+
+
+  auto t_end = chrono::high_resolution_clock::now();
+  chrono::duration<double> runtime = t_end - t_start; // runtime calc
+  cout << "Runtime: " << runtime.count() << "s\n";
 
   return 0;
 }
