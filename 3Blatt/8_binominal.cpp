3b1c09e728fb561dab0546a6c297c44e45e43d39
SagruH <sagru2899@googlemail.com>
2020-05-07 13:21:12 +0200

    Blatt3: Aufgabe 8 add binomial function

--- 3Blatt/8_binominal.cpp
+++ 3Blatt/8_binominal.cpp
@@ -8,14 +8,22 @@
 
 using namespace std;
 
+int binom_co(int n, int k) {
+  int sol;
+  if (k == 0 || n == k) {
+    return 1;
+  } else {
+    return (binom_co(n-1 , k-1 ) + binom_co(n-1 , k));
+  }
+}
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
+  int line = 10;
+  int sol = binom_co(8,6);
 
-
-  //code
-
+  cout << sol << endl;
 
 
   auto t_end = chrono::high_resolution_clock::now();
