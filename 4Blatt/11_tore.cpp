8b87ba9e9c98d59ad74e04eeb3ea99653941c6b9
SagruH <sagru2899@googlemail.com>
2020-05-14 17:40:00 +0200

    Blatt4: Aufgabe 12 done

--- 4Blatt/11_tore.cpp
+++ 4Blatt/11_tore.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 
 using namespace std;
@@ -36,10 +37,32 @@ int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
   int col = 11;
+  int l = 7;
+  int g = 0;
   vector<vector <int>> content;
   content = readfile("a11-toredat.sec",col);
-  cout << content[0][0];
 
+  vector<int> goals;
+  for (size_t i = 0; i < l; i++) {
+    for (size_t j = 1; j < col; j++) {
+      g += content[i][j];
+    }
+    goals.push_back(g);
+    g = 0;
+  }
+
+  int max = *max_element(goals.begin(), goals.end());
+
+  printf("TEAM  ||             GOALS               || SUM");
+  printf("\n------------------------------------------------------------");
+  for (size_t i = 0; i < l; i++) {
+    printf("\n%i  ||  ",content[i][0] );
+    for (size_t j = 1; j < col; j++) {
+      printf(" %i ",content[i][j] );
+    }
+    printf(" || %i | ",goals[i] );
+    if (goals[i] == max) printf("CHAMPION!");
+  }
 
   auto t_end = chrono::high_resolution_clock::now();
   chrono::duration<double> runtime = t_end - t_start; // runtime calc
