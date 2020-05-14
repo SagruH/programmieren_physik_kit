c7da5f1a9f580bfcdac205332c5cd20512512132
SagruH <sagru2899@googlemail.com>
2020-05-14 16:28:35 +0200

    Blatt4 Aufgabe 11 readfile not working

--- 4Blatt/11_tore.cpp
+++ 4Blatt/11_tore.cpp
@@ -5,19 +5,43 @@
 #include <cmath>
 #include <chrono>
 #include <fstream>
+#include <vector>
 
 
 using namespace std;
 
+int readfile(string filename, int n, int l) { //columns ; lines
+  fstream ffile;
+  ffile.open(filename, ios::in);
+
+  int a;
+  int i=0;
+  int j=0;
+  int content[n][l] = {0};
+
+  while (ffile >> a) {
+    content[i][j] = a;
+    i++;
+    if (i==n) {
+      i=0;
+      j++;
+    }
+  }
+
+  ffile.close();
+  return 0;
+}
+
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
+  /*
+  int col = 11;
+  int l = 7;
+  int content[col][l] = {0};
+  content = readfile("a11-toredat.sec",col,l);
+  */
 
-  fstream goalfile("a11-toredat.sec", ios_base::in);
-  int a;
-  while (goalfile >> a) {
-    printf("%i ", a);
-  }
 
   auto t_end = chrono::high_resolution_clock::now();
   chrono::duration<double> runtime = t_end - t_start; // runtime calc
