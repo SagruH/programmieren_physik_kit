1e718fd7a9178387335020d6b74d12d258946036
SagruH <sagru2899@googlemail.com>
2020-05-21 13:00:07 +0200

    5Blatt: Aufgabe13 matrix read

--- 5Blatt/13_LGS.cpp
+++ 5Blatt/13_LGS.cpp
@@ -4,15 +4,47 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
-
+#include <vector>
+#include <fstream>
 
 using namespace std;
+
+vector<vector<double>> readfile(string filename, int n) { //columns ; lines
+  fstream ffile;
+  ffile.open(filename, ios::in);
+  double a;
+  int i=0, j=0;
+  vector<vector <double>> content;
+  vector<double> temp;
+
+  while (ffile >> a) {
+    temp.push_back(a);
+    i++;
+    if (i==n) {
+      i=0;
+      content.push_back(temp);
+      temp.clear();
+    }
+  }
+
+  ffile.close();
+  return content;
+}
+
 
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
+  vector<vector<double>> a_matrix;
+  a_matrix = readfile("a13-lgs1.dat",8);
+  for (size_t i = 0; i < 8; i++) {
+    for (size_t j = 0; j < 9; j++) {
+      cout << " " << a_matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
 
-  
+
 
 
 
