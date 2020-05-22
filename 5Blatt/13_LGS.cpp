fefb9eab1579b70bc9b975bc3dcb2a368fc573af
SagruH <sagru2899@googlemail.com>
2020-05-22 18:21:47 +0200

    Blatt5 Aufgabe 13 comments

--- 5Blatt/13_LGS.cpp
+++ 5Blatt/13_LGS.cpp
@@ -38,27 +38,33 @@ vector<vector<double>> readfile(string filename, int n) { //columns ; lines
 
 //gibt LGS aus
 void printLGS(vector<vector<double>> matrix, int n, int m) {
+  printf("-----   LGS   -----\n");
   for (size_t i = 0; i < n; i++) {
     for (size_t j = 0; j < m; j++) {
       printf("  %5g  ",matrix[i][j] );
-      if (j == (m-1)) printf(" | %5g\n",matrix[n][i] );
+      if (j == (m-1)) printf(" | %6g\n",matrix[n][i] );
     }
   }
 }
 
 int main(int argc, char const *argv[]) {
-  auto t_start = chrono::high_resolution_clock::now();
+  //Pivotiserungs abfrage
+  cout << "Mit Pivotisierung? (y / n)";
+  string pv;
+  cin >> pv;
+  int pivot = pv=="y" ? 1 : pv=="n" ? 0 : 2;
+  if (pivot == 2) {
+    cout << "Das werte ich mal als nein...\n\n";
+    pivot = 0;
+  }
 
+  auto t_start = chrono::high_resolution_clock::now();
   vector<vector<double>> a_matrix;
 
   a_matrix = readfile("a13-lgs1.dat",8);
   printLGS(a_matrix,8,8);
 
-
-
-
-
-
+  //Dreiecksform
 
   auto t_end = chrono::high_resolution_clock::now();
   chrono::duration<double> runtime = t_end - t_start; // runtime calc
