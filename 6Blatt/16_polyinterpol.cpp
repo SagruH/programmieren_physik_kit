e76664db4ecf4a0f9f4279c8e3c99d7a95efa528
SagruH <sagru2899@googlemail.com>
2020-06-03 20:03:09 +0200

    Blatt6 Aufgabe16 funktion f(x)

--- 6Blatt/16_polyinterpol.cpp
+++ 6Blatt/16_polyinterpol.cpp
@@ -4,18 +4,47 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
-
+#include <vector>
+#include <fstream>
 
 using namespace std;
+
+//known functions:
+void printMatrix(vector<vector<double>> matrix, int n, int m);
+vector<vector<double>> readfile(string filename, int n);
+
+//new functions:
+double interpolynom(vector<vector<double>> &data, int n, double x){
+  double fx = 0;
+  double xi, xk;
+  double li;
+
+  for (int i = 0; i < n; i++) {
+    //calc Lagrage interpol
+    li = 1;
+    xi = data[i][0];
+    for (int k = 0; k < n; k++) {
+      xk = data[k][0];
+      li = k == i ? li : li*((x-xk)/(xi-xk));
+    }
+  //calc f(x)
+  fx += data[i][1]*li;
+  }
+  return fx;
+}
 
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
+  int x;
+  int n = 12;
+  string filename = "a16-interpol.dat";
+  vector<vector<double>> given_data = readfile(filename, 2);
 
-
-  //code
-
+  printf("Given data points:\n");
+  printMatrix(given_data, 2, n);
+  printf("\n\nEnter x.\nTo exit the program enter 0.\n");
 
 
   auto t_end = chrono::high_resolution_clock::now();
@@ -23,4 +52,37 @@ int main(int argc, char const *argv[]) {
   cout << endl << "Runtime: " << runtime.count() << "s\n";
 
   return 0;
+}
+
+
+
+vector<vector<double>> readfile(string filename, int n) { //columns ; lines
+  fstream ffile;
+  ffile.open(filename, ios::in);
+  double a;
+  int i=0, j=0;
+  vector<vector <double>> content;
+  vector<double> temp;
+
+  while (ffile >> a) {
+    temp.push_back(a);
+    i++;
+    if (i==n) {
+      i=0;
+      content.push_back(temp);
+      temp.clear();
+    }
+  }
+
+  ffile.close();
+  return content;
+}
+
+void printMatrix(vector<vector<double>> matrix, int m, int n) {
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < m; j++) {
+      cout << " " << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
