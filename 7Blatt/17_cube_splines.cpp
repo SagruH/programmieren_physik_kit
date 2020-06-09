e522555c9af211bacafc95dc35f9f43bdc8509e2
SagruH <sagru2899@googlemail.com>
2020-06-09 20:03:20 +0200

    Blatt7: Aufgabe17 berechnug lambdaj myj und dj

--- 7Blatt/17_cube_splines.cpp
+++ 7Blatt/17_cube_splines.cpp
@@ -14,17 +14,85 @@ using namespace std;
 void printMatrix(vector<vector<double>> matrix, int n, int m);
 vector<vector<double>> readfile(string filename, int n);
 
+//new functions
+//Berechnung von M
+vector<double> M_calc(vector<vector<double>>& data, int n){
+  vector<double> x = data[0];
+  vector<double> y = data[1];
+
+  vector<double> lambdaj, myj, dj;
+  double temp, temp1, temp2, temp3;
+
+  double lambda0 ,myn, d0, dn;
+  lambda0 = myn = d0 = dn = 0;
+  double my0 = 2;
+
+  vector<double> M_i;
+
+  //lambdaj
+  lambdaj.push_back(lambda0);
+  for (int j = 1; j <= n-1; j++) {
+    temp = (x[j+1] - x[j])/(x[j+1] - x[j-1]);
+    lambdaj.push_back(temp);
+  }
+  //lambdaj.push_back(1312); //DEBUG
+
+  //myj
+  myj.push_back(my0);
+  for (int j = 1; j <= n-1; j++) {
+    temp = (x[j] - x[j-1])/(x[j+1] - x[j-1]);
+    myj.push_back(temp);
+  }
+  myj.push_back(myn);
+
+  //dj
+  dj.push_back(d0);
+  for (int j = 1; j <= n-1; j++) {
+    temp1 = 6/(x[j+1]-x[j-1]);
+    temp2 = (y[j+1] - y[j])/(x[j+1] - x[j]);
+    temp3 = (y[j] - y[j-1])/(x[j] - x[j-1]);
+    temp = temp1*(temp2-temp3);
+    dj.push_back(temp);
+  }
+  dj.push_back(dn);
+
+  /*  //DEBUG
+  for (size_t i = 0; i <= n; i++) {
+    printf("l: %9g  m: %9g  d: %9g\n",lambdaj[i], myj[i], dj[i] );
+  }
+  */
+  return M_i;
+}
+
 
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
   int n = 12;
+  vector<double> x, y;
+  vector<vector<double>> data;
+  vector<double> M_i;
+
+  //print out given .dat file
   string filename = "a16-interpol.dat";
   vector<vector<double>> given_data = readfile(filename, 2);
-
   printf("Given data points:\n");
   printMatrix(given_data, 2, n);
+
+  //given_data to data (changes format)
+  for (size_t i = 0; i < n; i++) {
+    x.push_back(given_data[i][0]);
+  }
+  for (size_t i = 0; i < n; i++) {
+    y.push_back(given_data[i][1]);
+  }
+  data.push_back(x);
+  data.push_back(y);
+
+  M_i = M_calc(data,n); // berechnet den Vector Mi
+
+
 
 
 
