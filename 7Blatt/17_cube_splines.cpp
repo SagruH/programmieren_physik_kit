9ea1ddde800ef302929e25740cde31a083598960
SagruH <sagru2899@googlemail.com>
2020-06-09 17:20:37 +0200

    Blatt7: Aufgabe17 copy paste von Aufgabe 16

--- 7Blatt/17_cube_splines.cpp
+++ 7Blatt/17_cube_splines.cpp
@@ -4,17 +4,27 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <vector>
+#include <fstream>
 
 
 using namespace std;
+
+//known functions:
+void printMatrix(vector<vector<double>> matrix, int n, int m);
+vector<vector<double>> readfile(string filename, int n);
+
 
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
+  int n = 12;
+  string filename = "a16-interpol.dat";
+  vector<vector<double>> given_data = readfile(filename, 2);
 
-
-  //code
+  printf("Given data points:\n");
+  printMatrix(given_data, 2, n);
 
 
 
@@ -23,4 +33,36 @@ int main(int argc, char const *argv[]) {
   cout << endl << "Runtime: " << runtime.count() << "s\n";
 
   return 0;
+}
+
+
+vector<vector<double>> readfile(string filename, int n) { //columns ; lines
+  fstream ffile;
+  ffile.open(filename, ios::in);
+  double a;
+  int i=0, j=0;
+  vector<vector <double>> content;
+  vector<double> temp;
+
+  while (ffile >> a) {
+    temp.push_back(a);
+    i++;
+    if (i==n) {
+      i=0;
+      content.push_back(temp);
+      temp.clear();
+    }
+  }
+
+  ffile.close();
+  return content;
+}
+
+void printMatrix(vector<vector<double>> matrix, int m, int n) {
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < m; j++) {
+      cout << " " << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
