8341ff1f8d899fe631bb579fc4020ade11eb9324
SagruH <sagru2899@googlemail.com>
2020-06-22 15:52:11 +0200

    Blatt9 Aufgabe 22 quicksort ohne rekursion für 0ten Fall

--- 9Blatt/22_quicksort.cpp
+++ 9Blatt/22_quicksort.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <vector>
 #include <cstdlib>
+#include <algorithm>
 
 
 using namespace std;
@@ -18,19 +19,44 @@ void printv(vector<double> v) {
   return;
 }
 
+void quicksort(vector<double> &v, int s, int t) {
+  vector<double> hv1, hv2;
+  int ht = t;
+  double pve = v[s];
+  for ( s <= t; t--;) {
+    if (v[t] <= pve) {
+      hv1.push_back(v[t]);
+    } else if (v[t] > pve) {
+      hv2.push_back(v[t]);
+    }
+  }
+
+  v.erase (v.begin()+s, v.begin()+ht);
+  v.insert(v.begin()+s, hv1.begin(), hv1.end() );
+  v.insert((v.begin()+hv1.size()+s), hv2.begin(), hv2.end() );
+  return;
+}
 
 int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
-  int n_rand = 1000;
+  int n_rand = 20;
   vector<double> val;
+
+
 
   //Generate 1000 random numbers
   srand(time(NULL));
   for (size_t i = 0; i < n_rand; i++) {
     val.push_back((rand()/(double)RAND_MAX));
   }
-  printv(val);
 
+  val.clear(); val = {10,1,2,7,4,5,6,3,8,9,0,11,12,17,14,15,16,13,18,19}; //DEBUG
+  printv(val);
+  cout << endl << endl;
+
+  //quicksort
+  quicksort(val , 0 , n_rand);
+  printv(val);
 
 
 
