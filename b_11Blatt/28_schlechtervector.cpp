ade2b392bcd80cb89493e6ad61484ec6daf69680
SagruH <sagru2899@googlemail.com>
2020-07-08 16:59:01 +0200

    Aufgabe 28 add set func

--- b_11Blatt/28_schlechtervector.cpp
+++ b_11Blatt/28_schlechtervector.cpp
@@ -5,25 +5,18 @@
 #include <cmath>
 #include <chrono>
 #include <vector>
+#include <cstring>
 
 
 using namespace std;
 
 class vector_lite {
-  int size;
-  int a[];
+  int size = 0;
+  int* a = new int[size];
 public:
-  vector_lite () {
-    size = 0;
-    a[0];
-  }
+  vector_lite () {}
   vector_lite (int in[], int s) {
-    size = s;
-    int b[size];
-    for (size_t i = 0; i < s; i++) {
-      b[i] = in[i];
-    }
-    *a = *b;
+    set(in, s);
   }
   int get_size() {
     return size;
@@ -33,17 +26,33 @@ public:
     for (size_t i = 0; i < size; i++) {
       printf(" %3i ",a[i] );
       j++;
-      if (j == 9) {
+      if (j == 10) {
         j = 0;
         cout << endl;
       }
     }
+    cout << endl;
   }
-  void set(int in[], int s) {
-
+  void set(int *in, int newSize) {
+    int* new_arr = new int[newSize];
+    memcpy( new_arr , in, newSize * sizeof(int)) ; // dest , src , amount
+    size = newSize;
+    delete [] a;
+    a = new_arr;
   }
-  void in(int num, int pl) {
+  void in(int num, int pl) { //inserts at given polaces and moves rest to the left
+    size++;
+    int temp[size];
+    for (size_t i = 0; i < pl; i++) { // copy everything before element
+      temp[i] = a[i];
+    }
+    temp[pl] = num;
+    for (size_t i = pl+1; i < size; i++) { //copy everyting after
+      temp[i] = a[i];
+    }
 
+    //delete [] a;
+    //a = temp;
   }
   void del(int pl) {
 
@@ -56,7 +65,18 @@ int main(int argc, char const *argv[]) {
   auto t_start = chrono::high_resolution_clock::now();
 
   int test[15] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-  vector_lite at (test, 15);
+  int test2[10] = {2,5,7,0,4,3,99,5,12,169};
+
+  cout << "constuctor\n";
+  vector_lite at(test,15);
+  at.print();
+  cout << "\nset\n";
+  at.set(test2, 10);
+  at.print();
+  cout << "\nin\n";
+  //at.in(69,4);
+  //at.in(1312,0);
+  //at.in(420,18);
   at.print();
 
 
